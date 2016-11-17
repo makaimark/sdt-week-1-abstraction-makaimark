fe69e27557d8c595c5f5a547bb2471cfcdee5eee
makaimark <makaimark@gmail.com>
2016-11-17 21:29:45 +0100

    equals and hashCode functions generated for Poduct class

--- src/main/java/com/codecool/webshop/model/Product.java
+++ src/main/java/com/codecool/webshop/model/Product.java
@@ -8,4 +8,19 @@ public class Product {
 
     private int price;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        return id == product.id;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
 }
