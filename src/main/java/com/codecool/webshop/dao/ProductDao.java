5e34634f39b47c605adcf28dbf8c3996583ebbc7
makaimark <makaimark@gmail.com>
2016-11-18 12:59:59 +0100

    classes has add and list methods, without search method

--- src/main/java/com/codecool/webshop/dao/ProductDao.java
+++ src/main/java/com/codecool/webshop/dao/ProductDao.java
@@ -1,7 +1,15 @@
 package com.codecool.webshop.dao;
 
+import com.codecool.webshop.model.Product;
+
+import java.util.List;
+
 /**
  * Created by makaimark on 2016.11.17..
  */
 public interface ProductDao {
+
+    void add(Product p);
+
+    List<Product> list();
 }
--- src/main/java/com/codecool/webshop/dao/impl/InMemoryProductDao.java
+++ src/main/java/com/codecool/webshop/dao/impl/InMemoryProductDao.java
@@ -1,9 +1,27 @@
 package com.codecool.webshop.dao.impl;
 
 import com.codecool.webshop.dao.ProductDao;
+import com.codecool.webshop.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by makaimark on 2016.11.17..
  */
 public class InMemoryProductDao implements ProductDao {
+
+    private static final List<Product> DATA = new ArrayList<>();
+
+    @Override
+    public void add(Product p) {
+        DATA.add(p);
+    }
+
+    @Override
+    public List<Product> list() {
+        return DATA;
+    }
+
+    public Product search(Product p) { return  null;}
 }
