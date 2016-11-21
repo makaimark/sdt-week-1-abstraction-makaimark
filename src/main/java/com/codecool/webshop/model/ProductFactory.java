44e21a5dc26e465cd99a6486c46ab067bcb274f7
makaimark <makaimark@gmail.com>
2016-11-21 20:44:54 +0100

    product factory class

---
+++ src/main/java/com/codecool/webshop/model/ProductFactory.java
@@ -0,0 +1,11 @@
+package com.codecool.webshop.model;
+
+/**
+ * Created by makaimark on 2016.11.21..
+ */
+public class ProductFactory {
+
+    public Product getProduct() {
+        return new Product();
+    }
+}
