fda1a09f6687b6a89a9ebb171906c9b796bf929d
makaimark <makaimark@gmail.com>
2016-11-23 16:58:36 +0100

    table drop-build add, plusz jdbc connection

---
+++ src/main/java/com/codecool/webshop/dao/impl/ProductDaoInJDBC.java
@@ -0,0 +1,68 @@
+package com.codecool.webshop.dao.impl;
+
+import com.codecool.webshop.dao.ProductDao;
+import com.codecool.webshop.model.Product;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by makaimark on 2016.11.23..
+ */
+public class ProductDaoInJDBC implements ProductDao {
+
+    private static final String DATABASE = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String DB_USER = "makaimark";
+    private static final String DB_PASSWORD = "920410";
+
+    @Override
+    public void add(Product p) {
+        String query = "INSERT INTO todos (title, id, status) " +
+                "VALUES ('" + p.getId() + "', '" + p.getName() + "', '" + p.getPrice() + "');";
+        executeQuery(query);
+    }
+
+    @Override
+    public List<Product> list() {
+        String query = "SELECT * FROM afternoonsession;";
+
+        List<Product> resultList = new ArrayList<>();
+
+        try (Connection connection = getConnection();
+             Statement statement =connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+        ){
+            while (resultSet.next()){
+                Product actTodo = new Product(resultSet.getInt("id"),
+                        resultSet.getString("title"),
+                        resultSet.getInt("price"));
+                resultList.add(actTodo);
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return resultList;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                DATABASE,
+                DB_USER,
+                DB_PASSWORD);
+    }
+
+    private void executeQuery(String query) {
+        try (Connection connection = getConnection();
+             Statement statement =connection.createStatement();
+        ){
+            statement.execute(query);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/resources/database.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS afternoonsession;
+
+CREATE TABLE afternoonsession
+(
+id varchar(36) PRIMARY KEY,
+name varchar(40),
+price int
+);
